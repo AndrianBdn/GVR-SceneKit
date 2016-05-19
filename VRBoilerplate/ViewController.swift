e4bf76c828e67827f6e63da5ac8253bfe6df168d
Andrian Budantsov <andrian@readdle.com>
2016-05-19 10:48:59 +0300

    Initial Commit

---
+++ VRBoilerplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VRBoilerplate
+//
+//  Created by Andrian Budantsov on 5/19/16.
+//  Copyright © 2016 Andrian Budantsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
